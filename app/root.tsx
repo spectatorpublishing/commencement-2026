2b6362a32e0c9987be24d6bfbc88e8c5757773c9
Mussa Zeb <mussax@Mussas-MacBook-Pro-994.local>
2026-04-29 07:57:16 -0400

    mussa letter from editor component; finished building tsx and css structure; needs navbar integration and linked to a pages file (with credits)

--- app/root.tsx
+++ app/root.tsx
@@ -21,6 +21,10 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  {
+  rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Source+Serif+4:wght@700&family=Inter:wght@400&display=swap",
+  }
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
