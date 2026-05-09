891e79ce74c39dae3d8610db522fa46fdef78829
Lorraine Miller <lorraineqmiller@gmail.com>
2026-05-09 16:06:42 -0400

    Homepage background expanded and stars added

--- app/routes/Homepage.tsx
+++ app/routes/Homepage.tsx
@@ -1,10 +1,19 @@
+import UpperStars from "../assets/upper_stars.png"
+import LowerStars from "../assets/lower_stars.png"
+
 export default function Homepage() {
   return (
     <div
       style={{
         background: "linear-gradient(180deg, #F4A9CD 0%, #67A7CA 100%)",
-        minHeight: "calc(100vh - var(--nav-height))",
+        minHeight: 1024,
       }}
-    />
+    >
+      <img src={UpperStars} alt="" width="1485" height="836"
+        style={{top: -123}}/>
+      <img src={LowerStars} alt="" width="1485" height="836"
+        style={{top: 646}}/>
+      
+    </div>
   );
 }
