e3eaba7534450bb3dd1d1c45bca6389a8fdf2b14
Shriya <shriyamani@gmail.com>
2026-05-19 12:20:39 -0400

    few changes

--- app/data/sportsarticles.ts
+++ app/data/sportsarticles.ts
@@ -1,16 +1,34 @@
 const sports = [
     {
-      article_link: "https://www.columbiaspectator.com/sports/2025/10/06/unity-94-when-the-gay-games-came-to-columbia/",
-      image_url: "https://www.columbiaspectator.com/resizer/v2/GTB23ENXNRBVHNFCTE4PDIGXRI.png?auth=332543c7f6298b31bd4dd363916a0a195d37d56b5badb8aead2f8ab1b2d2fbe3",
-      article_title: "Unity ’94: When the Gay Games came to Columbia",
-      article_authors: "Arielle Rieder",
+      article_link: "https://www.columbiaspectator.com/sports/2026/05/11/run-your-own-race-the-legacy-of-senior-co-captain-susie-rafiu-on-womens-basketball/",
+      image_url: "https://www.columbiaspectator.com/resizer/v2/PE3O5DWUJZHOBP4ZE4HHXGL4YE.jpg?auth=9394c1876d52b00fee97c00b180415acd06a518bb1f7662c74da67363a47dc75",
+      article_title: "‘Run your own race’: The legacy of senior co-captain Susie Rafiu on women’s basketball",
+      article_authors: "Robby Swan",
     },
     {
-      article_link: "https://www.columbiaspectator.com/sports/2025/10/08/were-stronger-in-numbers-than-we-are-alone-charli-phillips-bc-26-on-athlete-ally/",
-      image_url: "https://www.columbiaspectator.com/resizer/v2/OQROJTOZGRB7VO5JPBR6KTF72E.jpeg?auth=20150d917cd5b6728026f883c908e52ffeb58fcd3afd1a02f17ca26a0b148f37",
-      article_title: "‘We’re stronger in numbers than we are alone’: Charli Phillips, BC ’26, on Athlete Ally",
+      article_link: "https://www.columbiaspectator.com/sports/2026/05/04/put-the-work-and-the-time-in-perri-page-cc-26-on-bringing-streetball-grit-to-the-ivy-league/",
+      image_url: "https://www.columbiaspectator.com/resizer/v2/BSHGV2VBLND3VKHFHQTNOWW25Y.jpg?auth=60b372fafea9e5ec544696dc88fce9c5526aec2386caacaa806be748fd025670",
+      article_title: "‘Put the work and the time in’: Perri Page, CC ’26, on bringing streetball grit to the Ivy League",
       article_authors: "Nicholas Fink",
-    }
+    },
+    {
+      article_link: "https://www.columbiaspectator.com/sports/2026/05/18/senior-guard-kenny-noland-hasnt-accomplished-all-his-goals-with-the-lions-but-he-wouldnt-have-it-any-other-way/",
+      image_url: "https://www.columbiaspectator.com/resizer/v2/MQVOFW4GZBGHHHHOOVLSGUHPGE.jpg?auth=57b1a43df4c88a374e83780de34ef8939168d7dc7f46f535cf2d8018e43768f2",
+      article_title: "Senior guard Kenny Noland hasn’t accomplished all his goals with the Lions, but he wouldn’t have it any other way",
+      article_authors: "Andrew Vincenti",
+    },
+    {
+      article_link: "https://www.columbiaspectator.com/sports/2026/05/11/rhodes-on-the-road-to-redemption-senior-foil-fencer-zander-rhodes-sets-her-sights-on-2028-olympics/",
+      image_url: "https://www.columbiaspectator.com/resizer/v2/QZS5EBETWBEEZIQ6XCEQJMTNHU.jpeg?auth=7acb78efda876c4d9367dc6835f104867f4f035ffe83cf2943f1014acd7e8258",
+      article_title: "Rhodes on the road to redemption: Senior foil fencer Zander Rhodes sets her sights on 2028 Olympics",
+      article_authors: "Emily Michaelsen",
+    },
+    {
+      article_link: "https://www.columbiaspectator.com/sports/2026/05/18/senior-wrestler-kai-owen-caps-historic-ivy-league-title-run-with-the-lions/",
+      image_url: "https://www.columbiaspectator.com/resizer/v2/JBBTOYKQDFDLFEPJFXDBGJQGKU.jpeg?auth=08329d6e1b6f30a66bcfd1c7aa724101064de2d4243f3ff85a50a6f8b2e6002c",
+      article_title: "Senior wrestler Kai Owen caps historic Ivy League title run with the Lions",
+      article_authors: "Anivartin Daga",
+    },
   ];
   
   export default sports;
