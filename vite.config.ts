1da32a34fbd3a405f6395f07e309c4994326d219
Shriya <shriyamani@gmail.com>
2026-05-19 09:31:42 -0400

    deployment info

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter()],
+  base: process.env.PUBLIC_URL || "/",
   resolve: {
     tsconfigPaths: true,
   },
